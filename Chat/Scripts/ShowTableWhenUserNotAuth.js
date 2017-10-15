5afa52ae859aa228d119c86ccdcc0afbb6cbd1b7
RostyslavZhovnir <zhovnir.info@gmail.com>
2017-10-14 23:11:32 -0500

    Css

---
+++ Chat/Scripts/ShowTableWhenUserNotAuth.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('.table').hide();
+    $('.hideArticles').hide();
+
+    $('.seeArticles').click(function () {
+        $('.table').show("fast")
+        $('.hideArticles').show()
+    });
+
+    $('.hideArticles').click(function () {
+        $('.table').hide("fast")
+        $('.hideArticles').hide();
+    });
+});
--- Chat/Scripts/util.js
+++ Chat/Scripts/util.js
@@ -3,6 +3,7 @@
 
 $(function () {
     
+   
 
 
     $(".table").on('click', '#joinGroup', function () {
